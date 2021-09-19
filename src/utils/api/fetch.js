949d629ac65d07ab1b1be6172756f07009b045c3
madiyar <madiyar.bolatov97@gmail.com>
2021-09-19 22:32:24 +0600

    CUMments

--- src/utils/api/fetch.js
+++ src/utils/api/fetch.js
@@ -1,10 +1,13 @@
 const baseUrl = 'http://173.249.20.184:7001/api';
 
 const http = async (url, options = { method: "GET" }) => {
+    // проверяем есть ли body в опциях
     const body = options?.body ? JSON.stringify(options.body) : "";
+    // если method не GET то добавляем body в запрос
     if (options.method !== "GET") {
         options = { ...options, body };
     }
+    // запрос к api
     const res = await fetch(`${baseUrl}${url}`, {
         ...options,
         method: options.method || 'GET'
